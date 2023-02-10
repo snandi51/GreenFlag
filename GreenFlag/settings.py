e97241001d7912e8f314735fd7dba81b885745c3
Vishal Capgemini <95024050+Vishal-Capgemini2121@users.noreply.github.com>
2023-02-10 10:46:03 +0530

    Update settings.py

--- GreenFlag/settings.py
+++ GreenFlag/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%ls1qpn5+-^r-bs7c(&^cejfg)k3kgopfcn&6(64wse7_89^ux'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
